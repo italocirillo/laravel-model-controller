74eb56d5d90b35e100773279fb39a8f75476f224
italocirillo <italocirillo@gmail.com>
2023-05-24 19:32:50 +0200

    ESERCIZIO COMPLETO + BONUS

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,26 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-    <h1>{{ $movies }}</h1>
-</body>
-
-</html>
+@section('content')
+    <div class="container py-3">
+        <div class="container text-center">
+            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 align-items-start">
+                @foreach ($movies as $movie)
+                    <div class="col py-3">
+                        <div class="card">
+                            <img src="{{ $movie['image'] }}" class="card-img-top" alt="{{ $movie['title'] }}">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $movie['title'] }}</h5>
+                                <p class="card-text">{{ $movie['original_title'] }}</p>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">{{ $movie['nationality'] }}</li>
+                                    <li class="list-group-item">Voto: {{ $movie['vote'] }}</li>
+                                    <li class="list-group-item">Data d'uscita: {{ $movie['date'] }}</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+@endsection
